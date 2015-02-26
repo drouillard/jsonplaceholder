8f3f0b1a648a5788b27b5befd92c33defbe5ffca
Douglas Drouillard <drouillard@gmail.com>
2015-02-25 19:30:58 -0500

    Adding buildings route

--- index.js
+++ index.js
@@ -3,9 +3,25 @@ var clone = require('clone')
 var data = require('./data.json')
 
 var port = process.env.PORT || 3000 
+var exphbs = require('express-handlebars');
 
 var app = jsonServer.create()
 var router = jsonServer.router(clone(data))
+
+app.set ('views', __dirname + '/views');
+app.engine('.hbs', exphbs({defaultLayout: 'layout', extname: 'html.hbs'}));
+app.set('view engine', 'hbs');
+
+app.get('/buildings/:id', function(req,res){
+      console.dir(req);
+     res.render('buildings/show', {
+        title: req.params.id
+    });
+});
+
+
+
+
 
 app.all('*', function(req, res, next) {
   router.db.object = clone(data)
